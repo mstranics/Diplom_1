3a274230bf51e9fe3d106e4d9f50533e49d5080b
mikhail.demin <mikhail.demin@dif.tech>
2024-09-15 22:38:18 +0200

    added all project files

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,2 +1,17 @@
-package Praktikum;public class BunTest {
+package praktikum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+
+    @Test
+    public void getNamePriceReturnCorrectValue() {
+        Bun bun = new Bun("Булочка", 5);
+        String actName = bun.getName();
+        float actPrice = bun.getPrice();
+        assertEquals("Булочка", actName);
+        assertEquals(5, actPrice, 0);
+    }
 }
